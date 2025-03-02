1df61a865494ab9b20a4411ddde107137752f9a7
Dell <Dell@DESKTOP-DQHMPDK>
2025-03-02 23:29:19 +0700

    Baihocmoi Duyên test

---
+++ Appc#/Baihoc.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Appc_
+{
+    internal class Baihoc
+    {
+        "Duyen"
+    }
+}
